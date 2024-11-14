64f8f558cb6f2d0c6c9884886e28f259aa5dbacd
Vladimir Eremin <personal@ermnvldmr.com>
2024-11-14 18:00:00 +0300

    [linux] Auto-commit: 2024-11-14T18:00:00+0300

--- .config/hypr/scripts/hyprpaper-random.sh
+++ .config/hypr/scripts/hyprpaper-random.sh
@@ -10,26 +10,45 @@ fi
 
 LOCK="$HOME/.lock/hyprpaper-random.lock"
 
+# Terminate any existing instance of this script
 if pids=$(lsof -t "$LOCK"); then
   kill -15 $pids
 fi
 
 (
 flock -n 100 || exit
-MONITOR=($(hyprctl monitors | grep Monitor | awk '{print $2}'))
-cache=""
+MONITOR=($(hyprctl -i 0 monitors | grep Monitor | awk '{print $2}'))
+wallpapers=()
+used_wallpapers=()
+
+# Function to populate the wallpapers array if empty
+load_wallpapers() {
+  wallpapers=($(find "${DIR}" -type f | shuf))
+}
+
+# Initial load of wallpapers
+load_wallpapers
 
 while true; do
-  if [[ $cache == $wallpaper ]]; then
-    wallpaper=$(find "${DIR}" -type f | shuf -n 1)
-  else
-    cache=$wallpaper
-    hyprctl -q hyprpaper unload all
-    hyprctl -q hyprpaper preload "$wallpaper"
-    for m in "${MONITOR[@]}"; do
-      hyprctl -q hyprpaper wallpaper "$m,$wallpaper"
-    done
-    sleep "$INTERVAL"
+  # Refill wallpapers list if all have been used
+  if [ ${#wallpapers[@]} -eq 0 ]; then
+    load_wallpapers
+    used_wallpapers=()
   fi
+
+  # Pick and remove the first wallpaper from the list
+  wallpaper="${wallpapers[0]}"
+  wallpapers=("${wallpapers[@]:1}")
+  used_wallpapers+=("$wallpaper")
+
+  # Set the wallpaper on all monitors
+  hyprctl -i 0 -q hyprpaper unload all
+  hyprctl -i 0 -q hyprpaper preload "$wallpaper"
+  for m in "${MONITOR[@]}"; do
+    hyprctl -i 0 -q hyprpaper wallpaper "$m,$wallpaper"
+  done
+
+  sleep "$INTERVAL"
 done
-) 100> $LOCK
+) 100> "$LOCK"
+
