06272ff3e20f7ca82f16ca837c396081705fc6fb
Vladimir Eremin <personal@ermnvldmr.com>
2024-12-02 12:00:01 +0300

    [linux] Auto-commit: 2024-12-02T12:00:01+0300

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -13,21 +13,46 @@ return {
     },
   },
   {
-    "nvimdev/dashboard-nvim",
+    "folke/snacks.nvim",
     event = "VimEnter",
     opts = function(_, opts)
-      local logo = [[
-__ __  ____  __ __  ____  __ __  ____   ____  __    __  __    __ 
-|  |  ||    ||  |  ||    ||  |  ||    | /    ||  |__|  ||  |__|  |
-|  |  | |  | |  |  | |  | |  |  | |  | |  o  ||  |  |  ||  |  |  |
-|  ~  | |  | |  ~  | |  | |  ~  | |  | |     ||  |  |  ||  |  |  |
-|___, | |  | |___, | |  | |___, | |  | |  _  ||  `  '  ||  `  '  |
-|     | |  | |     | |  | |     | |  | |  |  | \      /  \      / 
-|____/ |____||____/ |____||____/ |____||__|__|  \_/\_/    \_/\_/  
+      local header = [[
+ __ __  ____  __ __  ____   ____  __    __  __    __ 
+|  |  ||    ||  |  ||    | /    ||  |__|  ||  |__|  |
+|  ~  | |  | |  ~  | |  | |  o  ||  |  |  ||  |  |  |
+|___, | |  | |___, | |  | |  _  |\  `  '  /\  `  '  /
+|____/ |____||____/ |____||__|__|  \_/\_/    \_/\_/  
       ]]
 
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
+      opts.dashboard.preset.header = header
+      opts.dashboard.sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "colorscript -e square",
+          height = 5,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "hub status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" }
+      }
     end,
   },
   {
