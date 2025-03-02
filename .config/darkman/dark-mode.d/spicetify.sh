a43990f869a08d5151fe009bfbc42ac3c7460420
Vladimir Eremin <personal@ermnvldmr.com>
2025-03-02 19:58:33 +0300

    Auto-commit: 2025-03-02 19:58:33

---
+++ .config/darkman/dark-mode.d/spicetify.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+spicetify backup apply
+spicetify config current_theme catppuccin color_scheme mocha
+spicetify apply
---
+++ .config/darkman/light-mode.d/rofi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sed -i '/\/\*BEGIN_ROFI_THEME\*\//,/\/*END_ROFI_THEME\*\//c\/*BEGIN_ROFI_THEME*/\n@import "./light.rasi"\n/*END_ROFI_THEME*/' ~/.config/rofi/config/.theme.rasi
